4f21c06e330d3961e8337e6d53c7280904625797
ik04 <63468587+ik04@users.noreply.github.com>
2023-02-24 12:08:37 +0530

    defining routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,17 +1,34 @@
 const express = require("express")
 const router = express.Router()
+const Subscriber = require("../models/Subscribers")
 
 //CRUD
 //READ
-router.get("/", (req, res) => {
-  res.send("hello world")
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 router.get("/:id", (req, res) => {
   res.send(req.params.id)
 })
 
 //CREATE
-router.post("/", (req, res) => {})
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedTo: req.body.subscribedTo,
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
 //UPDATE
 router.patch("/:id", (req, res) => {})
 module.exports = router
